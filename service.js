1bf75318417bba60394f344db524f910468e5b4a
BrodyMacfarlane <macfarlanebrody@gmail.com>
2015-04-16 11:33:30 -0600

    yeeeee

--- service.js
+++ service.js
@@ -1,12 +1,18 @@
 var app = angular.module('userProfiles');
 
 app.service('mainService', function($http, $q) {
-
-
-  this.getUsers = function() {
-    return $http({
-        method: 'GET',
-        url: 'http://reqr.es/api/users?page=1'
+	var dfd = $q.defer();
+	this.getUsers = function() {
+	    $http({
+	        method: 'GET',
+	        url: 'http://reqr.es/api/users?page=1'
+    }).then(function(response){
+    	response = response.data.data
+    	for(i = 0; i < response.length; i++){
+    		response[i].first_name = 'Ralf'
+    	}
+    	dfd.resolve(response)
     })
-  }
+  	return dfd.promise;
+}
 });
